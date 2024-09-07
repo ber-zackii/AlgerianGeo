deabc3d152011f776f852235dcc13ecaf45075bb
ber-zackii <zakaria.berrani6@gmail.com>
2024-09-07 21:44:50 +0100

    update communes

--- ser.js
+++
@@ -1,5 +0,0 @@
-import {AlgeriaGeo} from 'algerian-geo';
-
-// Example usage
-console.log('Wilaya by ID 1:', AlgeriaGeo.getWilayaById(14));
-console.log('Commune by ID 1:', AlgeriaGeo.getCommuneById(1));
